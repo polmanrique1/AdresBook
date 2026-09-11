182a495285593be65ce1345b1faa30a2784601d4
pol <polmanriqueortiz@gmail.com>
2026-09-11 23:11:43 +0200

    app initializet

---
+++ main.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+
+typedef struct
+{
+    int phoneNumber;
+    char contactName[50];
+
+}  Contanct;
+
+void saveContact(Contanct *con){
+    FILE *file = fopen("data.txt", "ab");  
+    
+    if (file == NULL){
+        perror("Error");
+    }
+    
+    fprintf(file, "%d %s\n", con->phoneNumber, con->contactName);
+    fclose(file);
+}
+
+void createContact(const char *phoneNumber, const char *name){
+    Contanct con;
+    strncpy(con.contactName, name, 50 - 1);
+    con.contactName[sizeof(con.contactName-1)] = '\0';
+    con.phoneNumber = atoi(phoneNumber);
+
+    saveContact(&con);
+}
+
+void readContacts(){
+    FILE *file = fopen("data.txt", "r");
+
+    Contanct con;
+    while (fscanf(file, "%d %49s", &con.phoneNumber, con.contactName) == 2)
+    {
+        printf("Contact: %s | %d\n", con.contactName, con.phoneNumber);
+    }
+    
+    fclose(file);
+}
+
+
+int main(int argc, char *argv[]){
+
+    if (argc < 1) {
+        fprintf(stderr, "Uso: %s add <telefono> <nombre>\n", argv[0]);
+        return 1;
+    }
+
+    if (strcmp(argv[1], "add")== 0)
+    {
+        createContact(argv[2], argv[3]);
+    }
+    
+    if(strcmp(argv[1], "list")==0){
+        readContacts();
+    }
+
+    return 0;
+}
