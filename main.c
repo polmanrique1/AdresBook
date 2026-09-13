b0bf1e7a78ce46ff2395be10ec01f680b95d3b20
pol <polmanriqueortiz@gmail.com>
2026-09-13 12:37:43 +0200

    errors solved

--- main.c
+++ main.c
@@ -44,39 +44,57 @@ void readContacts(){
     fclose(file);
 }
 
-void readByPhoneNumber(int *number){
+void readByPhoneNumber(char *number){
     FILE *file = fopen("data.txt", "r");
 
     Contanct con;
-    while (fscanf(file,"%d %49s", &con.contactName, &con.phoneNumber)==2)
+    while (fscanf(file,"%d %49s", &con.phoneNumber, con.contactName)==2)
     {
-        if (con.phoneNumber == atoi(number))
+        if (con.phoneNumber== atoi(number))
         {
             printf("Contact: %s | %d\n", con.contactName, con.phoneNumber);
     
         }
     }
+
+    fclose(file);
 }
 
 void readByPhoneName(char *name){
     FILE *file = fopen("data.txt", "r");
 
     Contanct con;
-    while (fscanf(file,"%d %49s", &con.contactName, &con.phoneNumber)==2)
+    while (fscanf(file,"%d %49s", &con.phoneNumber, con.contactName)==2)
     {
-        if (con.contactName == name)
+        if (strcmp(con.contactName, name)==0)
         {
             printf("Contact: %s | %d\n", con.contactName, con.phoneNumber);
     
         }
     }
+
+    fclose(file);
 }
 
 void deleteByName(char *name){
     FILE *file = fopen("data.txt", "r");
-    FILE *file = fopen("data.txt", "r");
+    FILE *temp = fopen("temp.txt", "w");
 
-    
+
+    Contanct con;
+    while (fscanf(file, "%d %49s", &con.phoneNumber, con.contactName) == 2)
+    {
+        if (strcmp(con.contactName,name)!= 0)
+        {
+        fprintf(temp, "%d %s\n", con.phoneNumber, con.contactName);
+        }
+    }
+
+    fclose(temp);
+    fclose(file);
+
+    remove("data.txt");
+    rename("temp.txt", "data.txt");
 }
 
 
@@ -99,6 +117,12 @@ int main(int argc, char *argv[]){
     if(strcmp(argv[1], "get")==0){
         readByPhoneNumber(argv[2]);
     }
+
+    if (strcmp(argv[1], "r")==0)
+    {
+        deleteByName(argv[2]);
+    }
+    
 
     return 0;
 }
